352f4b386ca11cd9876d437be6126c5b0f5d615c
permalost <permalost@gmail.com>
2017-06-24 13:57:25 -0600

    Setting up basic Maven for a Processing 3 project

---
+++ src/main/java/com.norseamerican.autonomous.agents/MainApp.java
@@ -0,0 +1,22 @@
+package com.norseamerican.autonomous.agents;
+
+import processing.core.PApplet;
+
+public class MainApp extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("com.norseamerican.autonomous.agents.MainApp", args);
+    }
+
+    public void settings() {
+        size(900, 700);
+    }
+
+    public void setup() {
+        background(255);
+    }
+
+    public void draw() {
+        background(255);
+    }
+}
